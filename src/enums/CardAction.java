81acf58b7768b5da71da9053aab1224132c4367d
Rashed <rashedalmasri71@gmail.com>
2024-08-17 15:41:06 +0300

    changes on action factory

--- src/enums/CardAction.java
+++ src/enums/CardAction.java
@@ -1,10 +1,45 @@
 package enums;
 
+import engine.actions.*;
+import engine.actions.interfaces.Action;
+
 public enum CardAction {
-    None,
-    DrawFour,
-    DrawTwo,
-    SkipTurn,
-    ReverseTurn,
-    ChangeColor
+    None{
+        @Override
+        public Action getEffect(){
+            return new NoAction();
+        }
+    },
+    DrawFour{
+        @Override
+        public Action getEffect(){
+            return new DrawFour();
+        }
+    },
+    DrawTwo{
+        @Override
+        public Action getEffect(){
+            return new DrawTwo();
+        }
+    },
+    SkipTurn{
+        @Override
+        public Action getEffect(){
+            return new Skip();
+        }
+    },
+    ReverseTurn{
+        @Override
+        public Action getEffect(){
+            return new ReverseTurn();
+        }
+    },
+    ChangeColor{
+        @Override
+        public Action getEffect(){
+            return new ChangeColor();
+        }
+    };
+
+    public abstract Action getEffect();
 }
